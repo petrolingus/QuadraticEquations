635b37abe09cd2e41c4d0a7cdcdc632897fdea7b
Aleksandr A. Lebedev <aleksander.a.lebedev@netcracker.com>
2023-04-14 15:45:18 +0300

    solution fix

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,11 +1,12 @@
+#include <wsman.h>
 #include "quadratic.h"
 
 namespace quadratic {
 
     std::pair<double, double> calculate(double a, double b, double c) {
         double d = b * b - 4 * a * c;
-        double x1 = -b + sqrt(d) / (2 * a);
-        double x2 = -b - sqrt(d) / (2 * a);
+        double x1 = (-b + sqrt(d)) / (2 * a);
+        double x2 = (-b - sqrt(d)) / (2 * a);
         return std::make_pair(x1, x2);
     }
 
